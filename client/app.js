bbe684cb5a1c036aff88f8946af09ced0f5428f8
Camcan <mr.campbell.hawkes@gmail.com>
2016-09-28 15:36:27 +1300

    Impliment React Router

--- client/app.js
+++ client/app.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+import Code from './components/code.jsx'
+import About from './components/human.jsx'
 
 var App = React.createClass({
     render: function() {
         return ( 
         	<div>
-            <h1>{this.props.greeting}</h1> 
+            <h1>{this.props.greeting || 'SUUPER REACTIVE'}</h1> 
             </div>
         )
     }
 })
+
 ReactDOM.render( 
-	<App greeting = "FULLY REACTIVE" />,
+	<Router history={hashHistory}>
+	    <Route path="/" component={App}/>
+	    <Route path="/code" component={Code || App}/>
+	    <Route path="/about" component={About}/>
+ 	</Router>,
     document.getElementById('app')
 )
-console.log("I read")
+console.log( typeof Code )
+console.log("Bundle Success\n==============")
