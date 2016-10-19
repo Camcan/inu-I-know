b11a3a6e33b407813b8ccb2fa5b8b02699750c79
Camcan <mr.campbell.hawkes@gmail.com>
2016-10-19 18:34:48 +1300

    Add styley things, mouse tracking background on the home page

--- client/components/landing.jsx
+++ client/components/landing.jsx
@@ -4,9 +4,22 @@ import Styles from './css/landing.css'
 import Social from './social-links.jsx'
 
 export default React.createClass({
+	componentDidMount: function() {
+		var bg = document.getElementById('bg')
+		var view = window.innerWidth
+		console.log(bg)
+  		document.onmousemove = function(e){
+  			var x = e.clientX,
+        		y = e.clientY
+        		console.log("X:", x, "Y: ", y)
+        	bg.style.width =  (x + view/2 ) + "px"
+        	bg.style.transform = "rotate(" + y/20 + " deg)"
+  		}
+    },
 	render: function(){
 		return(
 			<div className={Styles.landing}>
+				<div id="bg" className={Styles.bg}></div>
 				<div id="title">            
 					<h1>{"I'm Cam"}</h1>
 					<h2>a web developer</h2>
