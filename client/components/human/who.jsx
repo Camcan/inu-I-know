45402129e9ca9fcb782bf9ea00324e31c7c639b8
Camcan <mr.campbell.hawkes@gmail.com>
2016-11-04 16:30:17 +1300

    Who page responsive

--- client/components/human/who.jsx
+++ client/components/human/who.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory, IndexRedirect, Link} from 'react-router'
 import Social from '../social-links.jsx'
-import HumanStyles from '../css/human.css'
+import Styles from '../css/who.css'
 
 export default React.createClass({
     render: function() {
         return ( 
-        	<div className={HumanStyles.center}>
+        	<div className={Styles.center}>
                     <h2>who?</h2>
-                <div className={HumanStyles.whoCol}>
+                <div className={Styles.whoCol}>
                     <p>Cam (Campbell Hawkes); 20, Web Developer</p>
                     <p>Completed the bootcamp provided by Enspiral Dev Academy, and have been doing stuff for and in agency with Enspiral for the past six months</p>
                 </div>
-                <div className={HumanStyles.whoCol}>
-        		      <div className={HumanStyles.profileImg}></div>
+                <div className={Styles.whoCol}>
+        		      <div className={Styles.profileImg}></div>
                     <p>Enjoying life in Wellington while immersing myself in different environments.</p>
                     <p>Currently looking for a full-time position.</p>
-                    <Social className={HumanStyles.socialButtons}/>
+                    <Social className={Styles.socialButtons}/>
                 </div>
         	</div>
         )
