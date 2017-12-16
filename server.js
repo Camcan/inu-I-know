7168f6422ec76fef2ca855261378bbc6af4df6bb
Developer <mr.campbell.hawkes@gmail.com>
2017-12-16 19:41:36 +1300

    Strip server ssl redirection & handle this via cloudflare

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var path = require('path')
 var express = require('express')
-var httpsRedirect = require('express-https-redirect')
 var app = express()
 
 app.set('port', (process.env.PORT || 3000))
 app.set('view engine', 'html')
-app.use('/', httpsRedirect(), express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
+
 app.locals.settings['x-powered-by'] = false
 
 app.get(['/de/resume'], function(req, res){
@@ -17,23 +17,23 @@ app.get(['/'], function(req, res) {
 })
 
 app.get(['/de/*'], function(req, res){
-   res.redirect('/')
-})
+               res.redirect('/')
+               })
 
 
 
 
 
 
-app.use(function(req, res) {
-      res.status(400);
-     res.sendFile(path.join(__dirname, 'client/views/404.html'));
- })
-app.use(function(error, req, res, next) {
-      res.status(500);
-     res.render('500.jade', {title:'Congratulations; you found a five-hundy error', error: error});
- })
+               app.use(function(req, res) {
+               res.status(400);
+               res.sendFile(path.join(__dirname, 'client/views/404.html'));
+               })
+               app.use(function(error, req, res, next) {
+               res.status(500);
+               res.render('500.jade', {title:'Congratulations; you found a five-hundy error', error: error});
+               })
 
-app.listen(app.get('port'), function() {
-  console.log('The wizardry is happening on port ' + app.get('port') + '/')
-})
+               app.listen(app.get('port'), function() {
+               console.log('The wizardry is happening on port ' + app.get('port') + '/')
+               })
