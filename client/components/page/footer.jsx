0679a158044ed2df06fb8033ce67d19b5955ba4b
mr.campbell.hawkes@gmail.com <mr.campbell.hawkes@gmail.com>
2018-06-06 18:53:57 +1200

    Refactor: move footer styles from base.css to components dir, tweak & run build

--- client/components/page/footer.jsx
+++ client/components/page/footer.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Router, Route, browserHistory, IndexRedirect, Link} from 'react-router'
+import React from 'react';
+import Styles from '../css/footer.css';
 
 export default React.createClass({
     render: function() {
         return ( 
-        	<div id="footer" className={"highlight"}>
-                <div className={"left"}>
+        	<div className={[
+                    Styles.footer, 
+                    "highlight"
+            ].join(" ")}>
+                <div className={Styles.left}>
                     <a href="mailto:campbell.hawkes@gmail.com">mr.campbell.hawkes@gmail.com</a>
                 </div>
-                <div className={"right"}>
+                <div className={Styles.right}>
                     <a href="https://github.com/camcan">camcan</a>
                 </div>
         	</div>
