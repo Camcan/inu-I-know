985ac6de884f4b288f4836af6f052591ad624ff5
Cam <mr.campbell.hawkes@gmail.com>
2020-06-19 00:11:13 +1200

    Glitch text has glitch prop as switch

--- client/components/assets/glitch-text.jsx
+++ client/components/assets/glitch-text.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import Styles from '../css/glitch.css';
+import React from "react";
+import Styles from "../css/glitch.css";
 
-export default function GlitchText({ content, element, onHover, className = '' }) {
-	const Element = element || 'div';
-	const glitchClass = Styles[onHover ? 'glitchOnHover' : 'glitch'];
-	
-	if (typeof content === 'string') return (
-		<Element className={`${glitchClass} ${Styles.glitchText} ${className} `} data-text={content}>
-			{content}
-		</Element>
-	);
-	
-	const Component = content;
-
-	return (
-		<Element className={className}>
-			<Component className={glitchClass} />
-			<Component className={Styles.glitchShadow} />
-		</Element>
-	);
+GlitchText.defaultProps = {
+  glitch: true
 };
+
+export default function GlitchText({
+  content,
+  element,
+  onHover,
+  className = "",
+  glitch
+}) {
+  const Element = element || "div";
+  const glitchClass = glitch
+    ? Styles[onHover ? "glitchOnHover" : "glitch"]
+    : undefined;
+
+  if (typeof content === "string") {
+    const glitchTextClass = glitch
+      ? `${glitchClass} ${Styles.glitchText}`
+      : undefined;
+    return (
+      <Element
+        className={`${glitchTextClass} ${className}`}
+        data-text={content}
+      >
+        {content}
+      </Element>
+    );
+  }
+
+  const Component = content;
+
+  return (
+    <Element className={className}>
+      <Component className={glitchClass} />
+      {glitch && <Component className={Styles.glitchShadow} />}
+    </Element>
+  );
+}
