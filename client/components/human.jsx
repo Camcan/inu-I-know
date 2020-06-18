218c074ba076de8584e8910553d79f8cf4284245
Cam <mr.campbell.hawkes@gmail.com>
2020-06-19 00:34:00 +1200

    Initial glitch title

--- client/components/human.jsx
+++ client/components/human.jsx
@@ -1,37 +1,67 @@
-import React,{Component} from 'react'
-import Styles from './css/human.css'
-import {router, Link, browserHistory} from 'react-router'
-import Nav from './page/nav.jsx'
-import GlitchText from './assets/glitch-text.jsx';
+import React, { Component } from "react";
+import Styles from "./css/human.css";
+import { router, Link, browserHistory } from "react-router";
+import Nav from "./page/nav.jsx";
+import GlitchText from "./assets/glitch-text.jsx";
 
-const navStyle = Styles.nav
-const hide = Styles.hide
+const navStyle = Styles.nav;
+const hide = Styles.hide;
+export default class Human extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default class Human extends Component{
-	constructor(props){
-		super(props)
-	}
-	navigate(){
-		const center = document.getElementsByClassName(Styles.center)[0];
-		center && center.classList.toggle(Styles.hide) 
+  state = {
+    glitch: false
+  };
 
-	}
-	render(){
-		return ( 
-			<div>
-				<div style={{opacity: 0 + "!important"}}className={Styles.head}>
-					<GlitchText className={Styles.logo} content={MontagnesLogo} />
-					<GlitchText element="h1" content="Cam; human" /> 
-					<Nav transition={this.navigate}/>
-				</div>
-				<div className={Styles.center}>
-					{this.props.children}
-				</div>
-			</div>
-		)
-	}
+  componentDidMount() {
+    this.toggleGlitch();
+  }
+
+  toggleGlitch() {
+    const { glitch } = this.state;
+    setTimeout(
+      () => this.toggleGlitch(),
+      glitch ? Math.random() * 10000 : 1000
+    );
+    this.setState({ glitch: !glitch });
+  }
+
+  navigate() {
+    const center = document.getElementsByClassName(Styles.center)[0];
+    center && center.classList.toggle(Styles.hide);
+  }
+
+  render() {
+    const { glitch } = this.state;
+    return (
+      <div className={Styles.root}>
+        <div className={Styles.head}>
+          <GlitchText
+            className={Styles.logoContainer}
+            content={MontagnesLogo}
+            glitch={glitch}
+          />
+          <GlitchText
+            element="h1"
+            content="Cam; human"
+            className={Styles.title}
+            glitch={glitch}
+          />
+          <Nav transition={this.navigate} />
+        </div>
+        <div className={Styles.center}>{this.props.children}</div>
+      </div>
+    );
+  }
 }
 
-function MontagnesLogo({ className }){
-	return <img className={className} src={"../../img/montagnes-white.svg"} />;
-};
+function MontagnesLogo({ className }) {
+  return (
+    <img
+      className={`${Styles.logo} ${className}`}
+      src={"../../img/montagnes-white.svg"}
+    />
+  );
+}
