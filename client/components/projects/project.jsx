6f4416deec8843c671f139e627fba20817f51689
Campbell Hawkes <mr.campbell.hawkes@gmail.com>
2020-07-20 23:53:57 +1200

    Adjust projects back button

--- client/components/projects/project.jsx
+++ client/components/projects/project.jsx
@@ -1,52 +1,33 @@
-import React, {Component} from 'react'
-import Styles from '../css/work.css'
-import Links from './projectLinks.jsx'
-import Back from '../assets/icons/back.js'
-import Web from '../assets/icons/earth.js'
-import Github from '../assets/icons/github.js'
-import NetworkContainer from '../network/index.js';
-import {Link, withRouter} from 'react-router-dom'
+import React from "react";
+import Styles from "../css/work.css";
+import Links from "./projectLinks.jsx";
+import Back from "../assets/icons/back.js";
+import NetworkContainer from "../network/index.js";
+import { Link, withRouter } from "react-router-dom";
 
-class Project extends Component {
-	constructor(props){
-        super(props);
-    }
-    clickTile(title){
-		this.setState({project: title})
-	}
-    render(){
-      var project = this.props
-      return ( 
-    		<div style={{height: 'auto'}}>
-                <Link className={Styles.backButton} 
-                    to="/work"
-                    onClick={()=>{
-                        this.props.back();
-                    }}
-                >   
-                    <Back/><span>go back</span>
-                </Link>
-                <div className={Styles.project}>
-                    <p className={Styles.projectTitle}>{project.title}</p>
-                    <Links github={project.github} live={project.live}/>
-                    <p className={Styles.blerb}>{project.description[0]}</p>
-                    <div className={Styles.mainImageContainer}>
-                    <img className={Styles.mainImage} src={"../../" + project.imgs[1]}/>
-                    </div>
-                    <div className={Styles.content}>
-                        <p className={Styles.descr}>
-                           {project.description[1]}
-                        </p>
-                        { 
-                           (project.imgs[2])? <img className={Styles.secondImage} src={"../../" + project.imgs[2]}/> : null
-                        }
-                        
-                    </div>
-                    {
-                        (project.title == 'Beer?') ? <NetworkContainer /> : null                    }
-                  </div>
-            </div>
-        )
-    }
-};
+function Project({ github, live, title, imgs, description, back }) {
+  return (
+    <div style={{ height: "auto" }}>
+      <Link className={Styles.backButton} to="/work" onClick={back}>
+        <Back />
+        <span>go back</span>
+      </Link>
+      <div className={Styles.project}>
+        <p className={Styles.projectTitle}>{title}</p>
+        <Links github={github} live={live} />
+        <p className={Styles.blerb}>{description[0]}</p>
+        <div className={Styles.mainImageContainer}>
+          <img className={Styles.mainImage} src={"../../" + imgs[1]} />
+        </div>
+        <div className={Styles.content}>
+          <p className={Styles.descr}>{description[1]}</p>
+          {imgs[2] ? (
+            <img className={Styles.secondImage} src={"../../" + imgs[2]} />
+          ) : null}
+        </div>
+        {title == "Beer?" ? <NetworkContainer /> : null}
+      </div>
+    </div>
+  );
+}
 export default withRouter(Project);
--- client/components/projects/work.jsx
+++ client/components/projects/work.jsx
@@ -1,74 +1,76 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import Tile from './tile.jsx';
-import Project from './project.jsx';
-import Styles from '../css/work.css';
-import DB from '../../db.json';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Tile from "./tile.jsx";
+import Project from "./project.jsx";
+import Styles from "../css/work.css";
+import DB from "../../db.json";
 
 export default class Work extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-                title: null,
-                project: null,
-                menu: true,
-                pageContents: this.getMenu
-        };
-        this.selectProject = this.selectProject.bind(this);
-        this.mouseoverTile = this.mouseoverTile.bind(this);
-        this.showMenu = this.showMenu.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: null,
+      project: null,
+      menu: true,
+      pageContents: this.getMenu
+    };
+    this.selectProject = this.selectProject.bind(this);
+    this.mouseoverTile = this.mouseoverTile.bind(this);
+    this.showMenu = this.showMenu.bind(this);
+  }
+  componentDidMount() {
+    this.setState({ projects: DB.projects });
+  }
+  mouseoverTile(title) {
+    this.setState({ title: title });
+  }
+  selectProject(project) {
+    window.scroll(0, 0);
+    this.setState({ project: project, menu: false });
+  }
+  showMenu() {
+    window.scroll(0, 0);
+    this.setState({ menu: true });
+  }
+  render() {
+    var titleStyle;
+    var pageContents;
+    if (this.state.projects) {
+      if (this.state.menu) {
+        titleStyle = Styles.myWork;
+        pageContents = (
+          <div className={Styles.tiles}>
+            {this.state.projects.map(project => (
+              <Link to={`/work/${project.title}`}>
+                <Tile
+                  project={project}
+                  passTitle={this.mouseoverTile}
+                  select={this.selectProject}
+                />
+              </Link>
+            ))}
+          </div>
+        );
+      } else {
+        titleStyle = Styles.myWorkProject;
+        pageContents = (
+          <Project
+            title={this.state.project.title}
+            description={this.state.project.desc}
+            imgs={this.state.project.imgs}
+            github={this.state.project.link.github}
+            live={this.state.project.link.live}
+            back={this.showMenu}
+          />
+        );
+      }
     }
-    componentDidMount() {
-        this.setState({projects: DB.projects })
-    }
-    mouseoverTile(title){
-        this.setState({title: title})
-    }
-    selectProject(project){
-        window.scroll(0,0);
-        this.setState({project: project,
-            menu: false
-        })
-    }
-    showMenu(){
-        window.scroll(0,0);
-        this.setState({menu: true});
-    }
-    render() {
-        var titleStyle = ""
-        if (this.state.projects) {
-            if (this.state.menu) {
-                titleStyle = Styles.myWork
-                this.state.pageContents = <div className={Styles.tiles}>
-                        {
-                            this.state.projects.map(project => (
-                                <Link to={`/work/${project.title}`}>
-                                    <Tile project={project} 
-                                        passTitle={this.mouseoverTile} 
-                                        select={this.selectProject                                                  }/>
-                                </Link>
-                            ))
-                        }
-                    </div>
-            } else {
-                titleStyle = Styles.myWorkProject
-                this.state.pageContents = <Project 
-                                            title={this.state.project.title}
-                                            description={this.state.project.desc}
-                                            imgs={this.state.project.imgs}
-                                            github={this.state.project.link.github}
-                                            live={this.state.project.link.live}
-                                            back={this.showMenu}
-                                        />
-            }
-        }
-        return ( 
-    		<div className={Styles.projectsContainer}>
-                <h2 className={titleStyle}>my work</h2>
-               {this.state.pageContents}
-               <h1 className={Styles.bgTitle}>{this.state.title}</h1>
-            </div>
-        )
-    }
-};
-
+    return (
+      <div className={Styles.projectsContainer}>
+        <h2 className={titleStyle}>my work</h2>
+        {pageContents}
+        <h1 className={Styles.bgTitle}>{this.state.title}</h1>
+      </div>
+    );
+  }
+}
